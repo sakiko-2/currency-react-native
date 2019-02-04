c87491fd9ca2e90a2ecad40181149f84ed4833b5
saki <16546851+sakiko-2@users.noreply.github.com>
2019-02-04 21:25:41 +1300

    Setup basic structure

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $primaryBlue: '#13b5ea',
+});
+
+export default () => <Home />;
