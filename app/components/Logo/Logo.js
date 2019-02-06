41d019b1e4a9e2bfd5b4540cf65a7f82dd143029
saki <16546851+sakiko-2@users.noreply.github.com>
2019-02-07 01:52:26 +1300

    Animate logo to be smaller when keyboard is open

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,22 +1,86 @@
-import React from 'react';
-import { View, Image, ImageBackground, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, View, ImageBackground, Keyboard, Platform, Text } from 'react-native';
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode='contain'
-      source={require('./images/background.png')}
-      style={styles.containerImage}
-    >
-      <Image
-        resizeMode='contain'
-        source={require('./images/logo.png')}
-        style={styles.image}
-      />
-    </ImageBackground>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+const ANIMATED_DURATION = 250;
+
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+  };
+
+  componentDidMount() {
+    let showListener = 'keyboardDidShow';
+    let hideListener = 'keyboardDidHide';
+
+    this.keyboardDidShowListener = Keyboard.addListener(showListener, this.keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener(hideListener, this.keyboardDidHide);
+  };
+
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  };
+
+  keyboardDidShow = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$smallContainerSize,
+        duration: ANIMATED_DURATION,
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$smallImageSize,
+        duration: ANIMATED_DURATION,
+      }),
+    ]).start();
+  };
+
+  keyboardDidHide = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$largeContainerSize,
+        duration: ANIMATED_DURATION,
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$largeImageSize,
+        duration: ANIMATED_DURATION,
+      }),
+    ]).start();
+  };
+
+  render() {
+    const containerImageStyle = [
+      styles.containerImage,
+      { width: this.containerImageWidth, height: this.containerImageWidth },
+    ];
+
+    const imageStyle = [
+      styles.logo,
+      { width: this.imageWidth },
+    ];
+
+    return (
+      <View style={styles.container}>
+        <Animated.View style={containerImageStyle}>
+          <ImageBackground
+            resizeMode='contain'
+            source={require('./images/background.png')}
+            style={styles.backgroundImage}
+          >
+            <Animated.Image
+              resizeMode='contain'
+              source={require('./images/logo.png')}
+              style={imageStyle}
+            />
+          </ImageBackground>
+        </Animated.View>
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+};
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,17 +4,27 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
   container: {
     alignItems: 'center'
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
+  },
+  backgroundImage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
   image: {
-    width: imageWidth / 2,
+    width: '$largeImageSize' / 2,
   },
   text: {
     color: '$white',
