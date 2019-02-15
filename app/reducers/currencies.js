5e22936f9dc9c31e06382962faf2e836b84b48a1
saki <16546851+sakiko-2@users.noreply.github.com>
2019-02-15 23:59:14 +1300

    Change currency

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,10 +1,77 @@
-import { CHANGE_CURRENCY_AMOUNT, REVERSE_CURRENCY, changeCurrencyAmount, reverseCurrency } from '../actions/currencies';
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  REVERSE_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY
+} from '../actions/currencies';
 
+// const initialState = {
+//   baseCurrency: 'NZD',
+//   quoteCurrency: 'JPY',
+//   amount: 1,
+//   conversion: {},
+// };
 const initialState = {
-  baseCurrency: 'NZD',
-  quoteCurrency: 'JPY',
-  amount: 1,
-  conversion: {},
+  baseCurrency: 'USD',
+  quoteCurrency: 'NZD',
+  amount: 100,
+  conversions: {
+    USD: {
+      isFetching: false,
+      base: 'USD',
+      date: '2017-05-31',
+      rates: {
+        AUD: 1.3416,
+        BGN: 1.743,
+        BRL: 3.2515,
+        CAD: 1.3464,
+        CHF: 0.97104,
+        CNY: 6.813,
+        CZK: 23.547,
+        DKK: 6.6302,
+        GBP: 0.77858,
+        HKD: 7.7908,
+        HRK: 6.6068,
+        HUF: 273.77,
+        IDR: 13308,
+        ILS: 3.5431,
+        INR: 64.463,
+        JPY: 110.86,
+        KRW: 1118.4,
+        MXN: 18.765,
+        MYR: 4.281,
+        NOK: 8.4117,
+        NZD: 1.4071,
+        PHP: 49.77,
+        PLN: 3.7173,
+        RON: 4.0687,
+        RUB: 56.774,
+        SEK: 8.6942,
+        SGD: 1.3829,
+        THB: 34.07,
+        TRY: 3.5366,
+        ZAR: 13.133,
+        EUR: 0.89119,
+      },
+    },
+  },
+};
+
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: '',
+    rates: {},
+  };
+
+  if (state.conversion[action.currency]) {
+    conversion = state.conversion[action.currency];
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversion,
+  };
 };
 
 const reducer = (state = initialState, action) => {
@@ -13,13 +80,25 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         amount: action.amount || 0,
-      }
+      };
     case REVERSE_CURRENCY:
       return {
         ...state,
-        baseCurrency: state.baseCurrency,
-        quoteCurrency: state.quoteCurrency,
-      }
+        baseCurrency: state.quoteCurrency,
+        quoteCurrency: state.baseCurrency,
+      };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action),
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action),
+      };
     default:
       return state;
   }
